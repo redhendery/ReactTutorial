9246a65e0507f9cdb30a8f1bedd38dc5bae98dfb
redhendery <redhendery@gmail.com>
2016-07-13 15:15:13 +1200

    config test ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const NODE_ENV = process.env.NODE_ENV;
 const dotenv = require('dotenv');
 
-const isDev = NODE_ENV === 'development';
-
 const webpack = require('webpack');
 const fs      = require('fs');
 const path    = require('path'),
@@ -11,16 +9,31 @@ const path    = require('path'),
 
 const getConfig = require('hjs-webpack');
 
+const isDev  = NODE_ENV === 'development';
+const isTest = NODE_ENV === 'test';
+
 const root    = resolve(__dirname);
 const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
 const dest    = join(root, 'dist');
 
 var config = getConfig({
+  isDev: isDev,
   in: join(src, 'app.js'),
   out: dest,
-  clearBeforeBuild: true
-})
+  html: function (context) {
+    return {
+      'index.html': context.defaultTemplate({
+        title: 'yelp-clone from fullstackreact.com',
+        publicPath: isDev ? 'http://localhost:3000/' : '',
+        meta: {
+          'name': 'fullstackreact yelp clone',
+          'description': 'A minimal yelp clone from the team behind the fullstackreact.com book'
+        }
+      })
+    }
+  }
+});
 
 // ENV variables
 const dotEnvVars = dotenv.config();
@@ -46,13 +59,13 @@ config.plugins = [
 ].concat(config.plugins);
 // END ENV variables
 
-// CSS Modules
+// CSS modules
 const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
 const matchCssLoaders = /(^|!)(css-loader)($|!)/;
 
 const findLoader = (loaders, match) => {
-  const found = loaders.filter(l => l &&
-      l.loader && l.loader.match(match));
+  const found = loaders.filter(l => l && l.loader && l.loader.match(match))
   return found ? found[0] : null;
 }
 // existing css loader
@@ -60,22 +73,64 @@ const cssloader =
   findLoader(config.module.loaders, matchCssLoaders);
 
 const newloader = Object.assign({}, cssloader, {
-    test: /\.module\.css$/,
-    include: [src],
-    loader: cssloader.loader
-      .replace(matchCssLoaders,
-      `$1$2?modules&localIdentName=${cssModulesNames}$3`)
-  })
-  config.module.loaders.push(newloader);
-  cssloader.test =
-    new RegExp(`[^module]${cssloader.test.source}`)
-  cssloader.loader = newloader.loader
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+})
+config.module.loaders.push(newloader);
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
 
-  config.module.loaders.push({
-    test: /\.css$/,
-    include: [modules],
-    loader: 'style!css'
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+// CSS modules
+
+// postcss
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+])
+// END postcss
+
+// Roots
+config.resolve.root = [src, modules]
+config.resolve.alias = {
+  'css': join(src, 'styles'),
+  'containers': join(src, 'containers'),
+  'components': join(src, 'components'),
+  'utils': join(src, 'utils'),
+
+  'styles': join(src, 'styles')
+}
+// end Roots
+
+// console.log(require('prettyjson').render(config));
+
+// Testing
+if (isTest) {
+  config.externals = {
+    'react/addons': true,
+    'react/lib/ReactContext': true,
+    'react/lib/ExecutionEnvironment': true,
+  }
+  config.module.noParse = /[/\\]sinon\.js/;
+  config.resolve.alias['sinon'] = 'sinon/pkg/sinon';
+
+  config.plugins = config.plugins.filter(p => {
+    const name = p.constructor.toString();
+    const fnName = name.match(/^function (.*)\((.*\))/)
+
+    const idx = [
+      'DedupePlugin',
+      'UglifyJsPlugin'
+    ].indexOf(fnName[1]);
+    return idx < 0;
   })
-// End CSS Modules
+}
+// End Testing
 
 module.exports = config;
