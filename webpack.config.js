2905645bf3fd5c8ca61b1cb4a9f44238200ba1c4
redhendery <redhendery@gmail.com>
2016-07-13 14:40:36 +1200

    config ENV's:

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const NODE_ENV = process.env.NODE_ENV;
+const dotenv = require('dotenv');
+
 const isDev = NODE_ENV === 'development';
 
 const webpack = require('webpack');
@@ -20,6 +22,31 @@ var config = getConfig({
   clearBeforeBuild: true
 })
 
+// ENV variables
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+// END ENV variables
+
+// CSS Modules
 const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
 const matchCssLoaders = /(^|!)(css-loader)($|!)/;
 
@@ -49,5 +76,6 @@ const newloader = Object.assign({}, cssloader, {
     include: [modules],
     loader: 'style!css'
   })
+// End CSS Modules
 
 module.exports = config;
